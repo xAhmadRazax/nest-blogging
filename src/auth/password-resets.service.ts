1f8ad9818f784f116ad38d03370d253310197219
Ahmad Raza <muhammad.ahmad.raza789@gmail.com>
2026-04-07 05:44:31 +0500

    refactor(auth): add email notification to existing password reset flow

--- src/auth/password-resets.service.ts
+++ src/auth/password-resets.service.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { eq, and, gt, isNull } from 'drizzle-orm';
 import ms from 'ms';
 import { InjectDb } from 'src/db/db.provider';
@@ -6,39 +6,17 @@ import { TypeConfigService } from 'src/config/type.config.service';
 import type { DB, Transaction } from 'src/db/client';
 import { HashingService } from './hashing.service';
 import { passwordResets } from './schemas/password-resets.schema';
+import { EmailService } from 'src/email/email.service';
 
+@Injectable()
 export class PasswordResetsService {
   constructor(
     @InjectDb() private readonly db: DB,
     private readonly hashingService: HashingService,
+    private readonly emailService: EmailService,
     private readonly configService: TypeConfigService,
     private readonly logger: Logger,
   ) {}
-  async create({ userId }: { userId: string }) {
-    const passwordResetToken = this.hashingService.generateCryptoToken();
-    const hashedPasswordResetToken =
-      this.hashingService.encryptCryptoToken(passwordResetToken);
-
-    const validUpTo = new Date(
-      Date.now() +
-        +ms(
-          this.configService.get('auth', { infer: true })!.tokensExpiry
-            .passwordResetsTokenExpiry,
-        ),
-    );
-    await this.db.insert(passwordResets).values({
-      userId,
-      token: hashedPasswordResetToken,
-      expiresAt: validUpTo,
-    });
-
-    return {
-      passwordResetToken,
-      tokenValidUpTo: validUpTo,
-      livedTime: this.configService.get('auth', { infer: true })!.tokensExpiry
-        .passwordResetsTokenExpiry,
-    };
-  }
 
   async find({
     hashedToken,
@@ -69,5 +47,50 @@ export class PasswordResetsService {
       .update(passwordResets)
       .set({ usedAT: new Date() })
       .where(eq(passwordResets.token, hashedToken));
+  }
+
+  async passwordResets({
+    userId,
+    email,
+    name,
+    url,
+  }: {
+    userId: string;
+    email: string;
+    name: string;
+    url: string;
+  }) {
+    const res = await this.create({ userId });
+    await this.emailService.sendPasswordResetEmail({
+      url: `${url}/${res.token}`,
+      recipientAddress: email,
+      recipientName: name,
+    });
+  }
+
+  private async create({ userId }: { userId: string }) {
+    const passwordResetToken = this.hashingService.generateCryptoToken();
+    const hashedPasswordResetToken =
+      this.hashingService.encryptCryptoToken(passwordResetToken);
+
+    const validUpTo = new Date(
+      Date.now() +
+        +ms(
+          this.configService.get('auth', { infer: true })!.tokensExpiry
+            .passwordResetsTokenExpiry,
+        ),
+    );
+    await this.db
+      .insert(passwordResets)
+      .values({
+        userId,
+        token: hashedPasswordResetToken,
+        expiresAt: validUpTo,
+      })
+      .returning();
+
+    return {
+      token: passwordResetToken,
+    };
   }
 }
